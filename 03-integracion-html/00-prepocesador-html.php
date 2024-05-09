2900cb2fdd5e3eb19a0442790bfcbb74478f5d20
darubis <yennidaruby@gmail.com>
2024-05-09 03:07:23 -0500

    PHP como preprocesador de HTML

---
+++ 03-integracion-html/00-prepocesador-html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP como un preprocesador de HTML</title>
+</head>
+<body>
+    <h1>Esto es html desde php</h1>
+    <?php
+        echo "<p>Ahora esto se esta imprimiendo con PHP 🎈</p><br><br><br>Otra cosa";
+    ?>
+</body>
+</html>
